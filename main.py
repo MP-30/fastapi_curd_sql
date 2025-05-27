efcee74b50c73c677623feb0dea0b89cb8f91375
mp-30 <adityabhadauriya93@gmail.com>
2025-05-27 13:28:30 +0000

    first commit

---
+++ main.py
@@ -0,0 +1,33 @@
+from typing import Optional
+from fastapi import FastAPI
+from fastapi.params import Body
+from pydantic import BaseModel
+from random import randrange
+
+
+app = FastAPI()
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
+
+my_posts = [{"title": "title of post 1", "content": "contect of post 1", "id": 1},{"title": "favorite food", "content": "I like pizza", "id": 2}]
+
+@app.get("/")
+async def root():
+    return {"message" : "Hello World"}
+
+
+@app.get("/posts")
+def get_post():
+    return {"data": my_posts}
+
+
+@app.post("/posts")
+def create_posts(post: Post):
+    post_dict = post.model_dump()
+    post_dict['id'] = randrange(1,1000000)
+    my_posts.append(post_dict)
+    return {"data": post_dict} 
